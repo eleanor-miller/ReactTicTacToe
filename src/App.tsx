2c5f201b07b740db951fd53db2afa9ce45828059
eleanor-miller <eleanormiller08@icloud.com>
2021-12-01 14:47:42 -0500

    Added the API to create a new game

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,29 @@ export function App() {
     console.log({ row, column })
   }
 
+  async function handleNewGame() {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+
+      // Make that the new state!
+      setGame(newGame)
+    }
+  }
+
   return (
     <div>
       <h1>
-        Tic Tac Toe - <button>New</button>
+        Tic Tac Toe - <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
         <li
